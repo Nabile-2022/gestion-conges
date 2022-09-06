2e5962d84c726e0a0e1c925bdb6fad901b8c979e
Nabile Rahmani <nrahmani@diginamic-formation.fr>
2022-09-06 16:53:09 +0200

    Added bootstrap and a navbar

---
+++ client/src/app/shared/nav-menu/nav-menu.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { AppComponent } from 'src/app/app.component';
+
+@Component({
+  selector: 'app-nav-menu',
+  templateUrl: './nav-menu.component.html',
+  styleUrls: ['./nav-menu.component.css']
+})
+export class NavMenuComponent implements OnInit
+{
+  title: string;
+  routes =
+  [
+    { path: '', name: 'Accueil' },
+  ];
+
+  constructor(private appComponent: AppComponent)
+  {
+    this.title = appComponent.title;
+  }
+
+  ngOnInit(): void
+  {
+  }
+}
