abe9c323fa268d6fcbd823d884a1ee2f75ccfe49
Nabile Rahmani <nrahmani@diginamic-formation.fr>
2022-09-07 10:57:10 +0200

    Server created

---
+++ server/src/main/java/gestion_conges/server/GestionCongesServerApplication.java
@@ -0,0 +1,13 @@
+package gestion_conges.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionCongesServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionCongesServerApplication.class, args);
+	}
+
+}
