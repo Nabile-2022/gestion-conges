1263d398a5003f5c3342fb5c6eeb738fecd35997
Jacques Granarolo <jac.granarolo.info@gmail.com>
2022-09-07 12:24:13 +0200

    mise en place

--- server/src/main/java/gestion_conges/server/GestionCongesServerApplication.java
+++ server/src/main/java/gestion_conges/server/GestionCongesServerApplication.java
@@ -8,6 +8,7 @@ public class GestionCongesServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GestionCongesServerApplication.class, args);
+		System.out.println("test ok");
 	}
 
 }
