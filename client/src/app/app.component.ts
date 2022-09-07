959a6b4622b01202efd70955f8352482776eef67
Nabile Rahmani <nrahmani@diginamic-formation.fr>
2022-09-07 12:54:04 +0200

    Hide NavMenu when logged out

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UserService } from './services/user.service';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,6 @@ import { Component } from '@angular/core';
 export class AppComponent
 {
   title = 'Gestion des congés';
+
+  constructor(public userService: UserService) { }
 }
