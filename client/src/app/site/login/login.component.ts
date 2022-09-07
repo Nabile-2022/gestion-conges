a0bafaa55bc11d9831869e5a5e1640cc91320465
Nabile Rahmani <nrahmani@diginamic-formation.fr>
2022-09-07 12:00:48 +0200

    Auth page mock

--- client/src/app/site/login/login.component.ts
+++ client/src/app/site/login/login.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Role } from 'src/app/models/role';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit
+{
+  roles = Object.keys(Role).filter(k => isNaN(Number(k)));
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void
+  {
   }
-
 }
