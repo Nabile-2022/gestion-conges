67db723d384652fae9e12a66a0fb1137ee8b2ad9
Jacques Granarolo <jac.granarolo.info@gmail.com>
2022-09-12 16:18:59 +0200

    added placeholder and FormBuilder

--- client/src/app/site/demande-absence/demande-absence.component.ts
+++ client/src/app/site/demande-absence/demande-absence.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Absence } from 'src/app/models/absence';
 
 @Component({
   selector: 'app-demande-absence',
@@ -8,14 +9,19 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class DemandeAbsenceComponent implements OnInit {
 
-  form!: FormGroup;
+  form: FormGroup;
+  absence!: Absence;
 
-  constructor(private formBuilder: FormBuilder)
-  {
+  constructor(private formBuilder: FormBuilder) {
     //this.form = formBuilder.group()
- /*this.form = formBuilder.group(
-
-  );*/
+    this.form = formBuilder.group(
+      {
+        startDate: "01/01/2022",
+        endDate: "01/01/2022",
+        typeConge: "Congé Payé",
+        motif: ""
+      }
+    );
   }
 
   ngOnInit(): void {
--- client/src/app/site/demande-absence/demande-absence.module.ts
+++ client/src/app/site/demande-absence/demande-absence.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DemandeAbsenceComponent } from './demande-absence.component';
 import { RouterModule, Routes } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 const routes: Routes =
 [
@@ -14,6 +15,7 @@ const routes: Routes =
   ],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     RouterModule.forChild(routes)
   ]
 })
