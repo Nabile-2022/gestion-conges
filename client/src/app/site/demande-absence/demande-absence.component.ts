8dacc1fa10f0eea338276d853d8e7a96d1792921
Jacques Granarolo <jac.granarolo.info@gmail.com>
2022-09-13 11:58:56 +0200

    fixed select type absence

--- client/src/app/site/demande-absence/demande-absence.component.ts
+++ client/src/app/site/demande-absence/demande-absence.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Absence } from 'src/app/models/absence';
+import { Role } from 'src/app/models/role';
 import { StatutAbsence } from 'src/app/models/statut-absence';
 import { TypeAbsence } from 'src/app/models/type-absence';
 
@@ -9,10 +10,12 @@ import { TypeAbsence } from 'src/app/models/type-absence';
   templateUrl: './demande-absence.component.html',
   styleUrls: ['./demande-absence.component.css']
 })
+
 export class DemandeAbsenceComponent implements OnInit
 {
   form: FormGroup;
   absence!: Absence;
+  types = Object.keys(TypeAbsence).filter(k => isNaN(Number(k)));
 
   constructor(private formBuilder: FormBuilder)
   {
