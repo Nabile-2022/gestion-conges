b0b9a39b40766419d24ded332d1d6322d05866d8
ckegb <kegbsuc@gmail.com>
2022-09-14 11:02:42 +0200

    Services

---
+++ server/src/main/java/gestion_conges/server/services/impl/AbsenceServiceImpl.java
@@ -0,0 +1,35 @@
+package gestion_conges.server.services.impl;
+
+import gestion_conges.server.entities.Absence;
+import gestion_conges.server.entities.Salarie;
+import gestion_conges.server.services.AbsenceService;
+
+import java.util.List;
+
+public class AbsenceServiceImpl implements AbsenceService {
+
+    @Override
+    public void addAbsence(Salarie salarie) {
+
+    }
+
+    @Override
+    public void deleteAbsence(int id) {
+
+    }
+
+    @Override
+    public Absence readAbsence(int id) {
+        return null;
+    }
+
+    @Override
+    public Absence updateAbsence(Absence absence, int id) {
+        return null;
+    }
+
+    @Override
+    public List<Absence> listerAbsence() {
+        return null;
+    }
+}
