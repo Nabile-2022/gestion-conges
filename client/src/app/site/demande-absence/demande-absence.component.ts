65f0efeee35be045c330cceba816aea870aa6aac
Jacques Granarolo <jac.granarolo.info@gmail.com>
2022-09-15 16:39:45 +0200

    validator form creation

--- client/src/app/site/demande-absence/demande-absence.component.ts
+++ client/src/app/site/demande-absence/demande-absence.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Absence } from 'src/app/models/absence';
 import { StatutAbsence } from 'src/app/models/statut-absence';
 import { TypeAbsence } from 'src/app/models/type-absence';
 import { typeAbsenceLabels } from 'src/app/localisation/french';
 import { AbsenceService } from 'src/app/services/absence.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { FormValidators } from '../shared/form-validators';
 
 @Component({
   selector: 'app-demande-absence',
@@ -15,8 +16,9 @@ import { ActivatedRoute, Router } from '@angular/router';
 
 export class DemandeAbsenceComponent implements OnInit
 {
-  form: FormGroup;
+  form!: FormGroup;
   absence!: Absence;
+
   typeAbsences = Object.values(TypeAbsence).filter(k => isNaN(Number(k)));
   typeAbsenceLabels = typeAbsenceLabels;
 
@@ -30,34 +32,37 @@ export class DemandeAbsenceComponent implements OnInit
       type: TypeAbsence.CongePaye,
       statut: StatutAbsence.Initiale
     };
-    this.form = formBuilder.group(
-      {
-        dateDebut: this.absence.dateDebut,
-        dateFin: this.absence.dateFin,
-        type: this.absence.type,
-        motif: this.absence.motif
-      }
-    );
+
 
   }
 
   ngOnInit(): void
   {
+
+
+
+    this.form = this.formBuilder.group(
+      {
+        dateDebut: [this.absence.dateDebut, { validators: [FormValidators.pastDate(this.absence.dateDebut)] }],
+        dateFin: [this.absence.dateFin, { validators: [FormValidators.pastDate(this.absence.dateFin)] }],
+
+        type: this.absence.type,
+        motif: this.absence.motif
+      }
+    );
   }
 
   // TODO use validators for the inputs?
 
   addAbsence(): void {
+    const nowDate = new Date();
     this.absence.dateDebut = this.form.controls['dateDebut'].value;
     this.absence.dateFin = this.form.controls['dateFin'].value;
     this.absence.motif = this.form.controls['motif'].value;
     this.absence.type = this.form.controls['type'].value;
 
-    const nowDate = new Date();
+
     let isOk = this.absence.dateFin > this.absence.dateDebut;
-    isOk = isOk && (new Date(this.absence.dateDebut) >= nowDate);
-    isOk = isOk && (new Date(this.absence.dateFin) > nowDate);
-    // TODO ajouter d'autres tests
 
     if(isOk) {
       this.absenceService.addAbsence(this.absence).subscribe(a => this.router.navigate(['..'], {relativeTo: this.route}));
