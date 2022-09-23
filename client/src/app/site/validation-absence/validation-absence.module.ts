0a1cd6b050fa50930fe608502c0a76b0df2010bd
Nabile Rahmani <nrahmani@diginamic-formation.fr>
2022-09-23 11:52:32 +0200

    Restore permission

--- client/src/app/site/validation-absence/validation-absence.module.ts
+++ client/src/app/site/validation-absence/validation-absence.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ValidationAbsenceComponent } from './validation-absence.component';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from 'src/app/guards/auth.guard';
 
 const routes: Routes =
-[
-  { path: 'validation-absences', component: ValidationAbsenceComponent }
-];
+  [
+    { path: 'validation-absences', component: ValidationAbsenceComponent, canActivate: [AuthGuard] }
+  ];
 
 @NgModule({
   declarations: [
@@ -15,6 +16,6 @@ const routes: Routes =
   imports: [
     CommonModule,
     RouterModule.forChild(routes)
-    ]
+  ]
 })
 export class ValidationAbsenceModule { }
